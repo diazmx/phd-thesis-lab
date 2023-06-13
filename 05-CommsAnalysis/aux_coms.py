33c6c0ee1622c9b5923b1a67466c3b6bee048f87
daniel <danieldiaz3000@gmail.com>
2023-06-12 22:44:11 -0600

    new changes

--- 05-CommsAnalysis/aux_coms.py
+++ 05-CommsAnalysis/aux_coms.py
@@ -18,6 +18,7 @@ def add_id_comm_to_nodes(user_network, commty_graph, id__comm):
         user_node = user_network.vs.find(name=user)
         user_node["commty"] = id__comm
 
+
 def get_community_graph(network, id_commty):
     """Get the igraph.Graph object of a community.
 
@@ -35,9 +36,10 @@ def get_community_graph(network, id_commty):
     seq_nodes.subgraph(): Graph (igraph)
         The igraph.Graph object of community with the `id_commty`.
     """
-    seq_nodes = network.vs.select(commty=id_commty) # Get all nodes.
-    
+    seq_nodes = network.vs.select(commty=id_commty)  # Get all nodes.
+
     return seq_nodes.subgraph()
+
 
 def get_all_resources_in_commty(commty_graph):
     """Get all resources that are accessed by all user in the `commty_graph`.
@@ -53,11 +55,11 @@ def get_all_resources_in_commty(commty_graph):
         The list with all resources that are accessed by all user in the commty.
     """
     # Store all resources, then convert to set to remove duplicates
-    all_resources = [] 
-    
-    for user_node in commty_graph.vs: # Loop over all users in the commty
+    all_resources = []
+
+    for user_node in commty_graph.vs:  # Loop over all users in the commty
         all_resources += user_node["rsrcs"]
-    
+
     # Remove duplicates
     all_resources = list(set(all_resources))
     return all_resources
@@ -85,43 +87,43 @@ def sub_community_detection(user_network, prev_partition, density_t=0.5):
         and (2) a list with all the resources that the users of the community
         access.
 
-    """    
+    """
     # Create a copy of the user_network
     # copy_user_network = user_network
     # Commts = Communities
     # Commty = Community
 
-    n_commts = len(set(user_network.vs["commty"])) # Get all previous commts
-    commty_counter = 0 # A counter to assign an ID to each commty detected
+    n_commts = len(set(user_network.vs["commty"]))  # Get all previous commts
+    commty_counter = 0  # A counter to assign an ID to each commty detected
 
-    # Dictionary to store all commts detected. An example: 
+    # Dictionary to store all commts detected. An example:
     # {id_commty: [subgraph, resources list]}
     dict_commts = {}
 
-    for id_commty in range(n_commts): # Loop over id of previous commts 
+    for id_commty in range(n_commts):  # Loop over id of previous commts
         # Get the Graph object of the community
         graph_commty = get_community_graph(user_network, id_commty)
 
         # Compute the density values of the commty
-        if graph_commty.density() < density_t: # If the network is sparse
+        if graph_commty.density() < density_t:  # If the network is sparse
 
             # Execute Louvain algorithm
             new_partition = graph_commty.community_multilevel(
-                weights = graph_commty.es["weight"] )
-            
-            for sub_commty in new_partition.subgraphs(): # Loop over new partition
+                weights=graph_commty.es["weight"])
+
+            for sub_commty in new_partition.subgraphs():  # Loop over new partition
                 # Get all resources that are accessed by the commty
-                all_rescs_commty = get_all_resources_in_commty(sub_commty) 
-                id_commty_str = str(commty_counter) # Convert the id to str
+                all_rescs_commty = get_all_resources_in_commty(sub_commty)
+                id_commty_str = str(commty_counter)  # Convert the id to str
                 # Add the new community to the dict
                 dict_commts[id_commty_str] = [sub_commty, all_rescs_commty]
                 # Add new ID commty to the user in user network
                 add_id_comm_to_nodes(user_network, sub_commty, id_commty_str)
                 commty_counter += 1
-        else: # If the network is dense
+        else:  # If the network is dense
             # Get all resources that are accessed by the commty
             all_rescs_commty = get_all_resources_in_commty(graph_commty)
-            id_commty_str = str(commty_counter) # Convert the id to str
+            id_commty_str = str(commty_counter)  # Convert the id to str
             # Add the new community to the dict
             dict_commts[id_commty_str] = [graph_commty, all_rescs_commty]
             # Add new ID commty to the user in user network
@@ -131,7 +133,49 @@ def sub_community_detection(user_network, prev_partition, density_t=0.5):
     return dict_commts
 
 
+def add_type_commty(network, id_commty, id_type):
+    """Add the type of the community (0: Sparse, 1: Medium, 2: Concentrated) to
+    each node.
+
+    Parameters
+    ----------
+    network: Graph (igraph)
+        Original network.
+    id_commty: int
+        The id of the community to add its type.
+    id_type: int
+        The type value of the community.
+
+    """
+    # Select all nodes in the id_commty
+    commty_nodes = network.vs.select(commty=id_commty)
+
+    for user in commty_nodes:
+        user["tpcommty"] = id_type
 
 
+def add_type_commts(network, dict_commts, s_threshold, m_threshold):
+    """ Add the id of the type of the commty.
 
+    """
+    s_commts = m_commts = c_commts = []  # lists to return
+    network.vs["tpcommty"] = -1
 
+    for commty in dict_commts.items():
+        if len(commty[1][1]) > s_threshold:  # Sparse commts
+            s_commts.append(commty)
+            add_type_commty(network, commty[0], 0)
+        elif len(commty[1][1]) > m_threshold:  # Medium commts
+            m_commts.append(commty)
+            add_type_commty(network, commty[0], 1)
+        else:
+            c_commts.append(commty)
+            add_type_commty(network, commty[0], 2)
+
+    print("# Comunidades:", len(s_commts)+len(m_commts)+len(c_commts), "==",
+          len(dict_commts))
+    print("Big Comms:", len(s_commts))
+    print("Med Comms:", len(m_commts))
+    print("Sma Comms:", len(c_commts))
+
+    return s_commts, m_commts, c_commts
