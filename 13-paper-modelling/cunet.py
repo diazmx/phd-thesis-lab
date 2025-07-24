092ae07e63d4bed4fde5c924be15a0eed7dad288
diazmx <danieldiaz3000@gmail.com>
2025-07-24 10:20:03 -0600

    asdasd

--- 13-paper-modelling/cunet.py
+++ 13-paper-modelling/cunet.py
@@ -7,19 +7,19 @@ import os
 
 print(f"using networkx version {nx.__version__}")
 
-G = nx.read_graphml("user-movie-lens.graphml")
+G = nx.read_graphml("binet-AMZ-Rw.graphml")
 print("|N|",G.number_of_nodes(), "   |M|",G.number_of_edges())
 
 nxcg_G = nxcg.from_networkx(G) 
 
 a = time()
 #bc_results = nx.betweenness_centrality(G, k=100)
-nx.betweenness_centrality(nxcg_G, k=100)
+nx.betweenness_centrality(nxcg_G, k=1000)
 b = time() - a
 print("GPU - time: %.10f seconds." % b)
 
 a = time()
-bc_results = nx.betweenness_centrality(G, k=1)
+bc_results = nx.betweenness_centrality(G, k=1000)
 #nx.betweenness_centrality(nxcg_G, k=1000)
 b = time() - a
 print("CPU - time: %.10f seconds." % b)
